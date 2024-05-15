8f403e4c68e1b576deff6470904fad17693469b0
afaqibrar <afaqibrar468@gmail.com>
2024-05-15 23:17:59 +0500

    vercel deployments

--- services/authService.js
+++ services/authService.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (res , userId) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET,{expiresIn:process.env.JWT_TTL});
+    console.log('tesr',process.env.NODE_ENV !== 'development');
+    
     res.cookie('jwt',token,{
        httpOnly:true,
        secure: process.env.NODE_ENV !== 'development',
