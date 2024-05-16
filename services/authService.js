0017fbac95fec114e6aa79db0109ca5044525f8c
afaqibrar <afaqibrar468@gmail.com>
2024-05-16 19:52:21 +0500

    Cookies issues implement the bearer token

--- services/authService.js
+++ services/authService.js
@@ -2,12 +2,10 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (res , userId) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET,{expiresIn:process.env.JWT_TTL});
-    console.log('tesr',process.env.NODE_ENV !== 'development');
-    
     res.cookie('jwt',token,{
        httpOnly:true,
        secure: process.env.NODE_ENV !== 'development',
-       sameSite: 'none',
+       sameSite: 'strict',
        maxAge: 30*24*60*60*1000 // seconds of 30 days
     })
     return token;
