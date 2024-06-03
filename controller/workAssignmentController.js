bff104640f041a0f2fa9a98162a26c1f2760d002
afaqibrar <afaqibrar468@gmail.com>
2024-06-03 15:05:02 +0500

    Assign Date issue

--- controller/workAssignmentController.js
+++ controller/workAssignmentController.js
@@ -3,61 +3,61 @@ const WorkAssignment = require('../models/workAssignmentModel');
 
 
 const getAllWork = asyncHandler(
-    async(req,res) => {
-        const works = await WorkAssignment.find({}).sort({createdAt: -1}).populate({
+    async (req, res) => {
+        const works = await WorkAssignment.find({}).sort({ createdAt: -1 }).populate({
             path: 'processLotId',
-            select: '_id pageNumber articleNumber colour billNumber quantity' 
+            select: '_id pageNumber articleNumber colour billNumber quantity'
         }).populate({
-            path:'workerId',
+            path: 'workerId',
             select: '_id workerName workerType'
-        });        
+        });
         res.status(200).json(works);
     }
 );
 const getWorkById = asyncHandler(
-    async(req,res) => {
-        const works = await WorkAssignment.find({workerId:req.params.id}).sort({createdAt: -1}).populate({
+    async (req, res) => {
+        const works = await WorkAssignment.find({ workerId: req.params.id }).sort({ createdAt: -1 }).populate({
             path: 'processLotId',
-            select: '_id pageNumber articleNumber colour billNumber quantity' 
+            select: '_id pageNumber articleNumber colour billNumber quantity assignDate'
         }).populate({
-            path:'workerId',
+            path: 'workerId',
             select: '_id workerName workerType'
         });
         res.status(200).json(works);
     }
 );
 
-const  createWork = asyncHandler( async(req,res) => {
-    const {processLotId,workerId} = req.body;
-    const workExist = await WorkAssignment.findOne({processLotId,workerId});    
-    if(workExist) {
+const createWork = asyncHandler(async (req, res) => {
+    const { processLotId, workerId } = req.body;
+    const workExist = await WorkAssignment.findOne({ processLotId, workerId });
+    if (workExist) {
         res.status(400);
         throw new Error('Work Already exist with this data');
     }
-    const work = await WorkAssignment.create({processLotId,workerId});
-        if(!work) {
+    const work = await WorkAssignment.create({ processLotId, workerId });
+    if (!work) {
         res.status(400);
         throw new Error('Invalid Work Data');
     }
-    res.status(200).json({work});
+    res.status(200).json({ work });
 });
 
-const deleteWork = asyncHandler(async(req,res) => {
+const deleteWork = asyncHandler(async (req, res) => {
     const work = await WorkAssignment.findById(req.params.id);
-    if(!work) {
+    if (!work) {
         res.status(400);
         throw new Error('Work not found');
     }
     await work.deleteOne();
-    res.status(200).json({id:req.params.id});
+    res.status(200).json({ id: req.params.id });
 });
 
 
-const updateWork = asyncHandler(async(req,res) => {
+const updateWork = asyncHandler(async (req, res) => {
     const existingWork = await WorkAssignment.findById(req.params.id).populate({
-            path: 'processLotId',
-            select: '_id quantity',
-            options: { lean: false } 
+        path: 'processLotId',
+        select: '_id quantity',
+        options: { lean: false }
     });
     if (!existingWork) {
         res.status(400);
@@ -75,4 +75,4 @@ const updateWork = asyncHandler(async(req,res) => {
     });
 });
 
-module.exports = {getAllWork,getWorkById,createWork,deleteWork,updateWork};
+module.exports = { getAllWork, getWorkById, createWork, deleteWork, updateWork };
